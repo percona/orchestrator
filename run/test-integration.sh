d1a48c5160ab16d1ec73e69910c3523edc659917
Kamil Holubicki <kamil.holubicki@percona.com>
2025-07-16 09:27:54 +0200

    DISTMYSQL-530: Update go version
    https://perconadev.atlassian.net/browse/DISTMYSQL-530
    
    Updated default PS tarball in test scripts

--- run/test-integration.sh
+++ run/test-integration.sh
@@ -7,7 +7,7 @@ export CI_ENV_REPO=
 export CI_ENV_BRANCH=
 
 # Configure test run parameters
-export TARBALL_URL=https://downloads.percona.com/downloads/Percona-Server-8.4/Percona-Server-8.4.3-3/binary/tarball/Percona-Server-8.4.3-3-Linux.x86_64.glibc2.31-minimal.tar.gz
+export TARBALL_URL=https://downloads.percona.com/downloads/Percona-Server-8.4/Percona-Server-8.4.5-5/binary/tarball/Percona-Server-8.4.5-5-Linux.x86_64.glibc2.31-minimal.tar.gz
 export RUN_TESTS=YES
 export ALLOW_TESTS_FAILURES=YES
 
